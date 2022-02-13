380cc830d5308c02615753d747ae777407f12e8d
Alexandre Destoky <adestoky@gmail.com>
2022-02-13 12:06:00 +0100

    Work with Class

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 "use strict";
-class Departement {
-    constructor(n) {
-        this.name = n;
+class Department {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        // private readonly id: string;
+        // private name: string;
+        this.employees = [];
+        // this.id = id;
+        // this.name = n;
+    }
+    describe() {
+        console.log(`Department (${this.id}): ${this.name}`);
+    }
+    addEmployee(employee) {
+        // validation etc
+        // this.id = 'd2';
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
-const accounting = new Departement("Accounting");
+const accounting = new Department('d1', 'Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+// accounting.employees[2] = 'Anna';
+accounting.describe();
+accounting.name = 'NEW NAME';
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe();
